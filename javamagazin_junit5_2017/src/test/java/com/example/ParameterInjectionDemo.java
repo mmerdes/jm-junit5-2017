5fd6dd941f1bd18281add5af8296a217b354de5a
Matthias Merdes <Matthias.Merdes@heidelberg-mobil.com>
2017-10-13 13:15:00 +0200

    parameter injection, test reporting

--- javamagazin_junit5_2017/src/test/java/com/example/ParameterInjectionDemo.java
+++ javamagazin_junit5_2017/src/test/java/com/example/ParameterInjectionDemo.java
@@ -4,14 +4,20 @@ package com.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 
 
-class TestInfoDemo {
+class ParameterInjectionDemo {
 
     @Test
     @DisplayName("my name")
     void aTest(TestInfo info) {
         System.out.println(info);
+    }
+
+    @Test
+    void reporting(TestReporter reporter) {
+        reporter.publishEntry("balance", "47.11");
     }
 
 
